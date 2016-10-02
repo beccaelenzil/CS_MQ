0235b307c456554afbd7ee742999f6c700e34c7d
oettam21 <matthew.querdasi@bush.edu>
2016-10-02 14:39:09 -0700

    Multiplication Game Enhancement

--- Multiplication Practice Game.py
+++ Multiplication Practice Game.py
@@ -1,30 +1,42 @@
 import random
 
-factor1 = random.randint(0,12)
-factor2 = random.randint(0,12)
 
 def play():
     userAnswer = 145
     correctAnswer = 146
+    gameCount = 0
     score = 0
-    while userAnswer != correctAnswer:
-        userAnswer = raw_input('Please enter the product of ' + str(factor1) + ' and ' + str(factor2))
-        correctAnswer = factor1 * factor2
-        userAnswer = int(userAnswer)
-        if userAnswer == (factor1*factor2):
-            print ('Correct!')
-        else:
-            print ('Wrong!')
-            print ('Try again!')
+    for gameCount in range(0,6):
+        if userAnswer != correctAnswer or userAnswer == correctAnswer:
+            factor1 = random.randint(0,12)
+            factor2 = random.randint(0,12)
+            userAnswer = raw_input('Please enter the product of ' + str(factor1) + ' and ' + str(factor2))
+            correctAnswer = factor1 * factor2
+            userAnswer = int(userAnswer)
+            if userAnswer == (factor1*factor2):
+                score = score + 1
+                print ('Correct!')
+                gameCount = gameCount + 1
+                print gameCount
+            else:
+                score = score - 2
+                print ('Wrong!')
+                print ('Try again!')
+                gameCount = gameCount + 1
+                print gameCount
+    print 'Your score is ' + str(score)
 def instructions():
     print 'Hey there sailor!'
     print 'Every correct answer gets you 1 point'
     print 'Every incorrect answer gets you -2 points'
     print ' '
-def name():
-    askName = raw_input(name)
+def endScore():
+    print 'Nice job!'
+    print 'Your total score is '
+    print int(score)
 def main():
-    name()
     instructions()
     play()
+    endScore()
 main()
+# GET HELP ON MAKING THE SCORE PRINT AT THE END!!!!!!!!*********
