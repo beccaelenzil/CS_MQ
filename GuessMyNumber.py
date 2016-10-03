8cf85292a0c5ed1dd5fec3f7d77ca25ca84f8832
oettam21 <matthew.querdasi@bush.edu>
2016-10-03 12:40:11 -0700

    GuessMyNumberGame

---
+++ GuessMyNumber.py
@@ -0,0 +1,11 @@
+import random
+
+def play():
+    gamecount = 0
+    if userAnswer != correctAnswer:
+        GITnumber = random.randint(0,101)
+        userAnswer = raw_input('Whats your guess? ')
+
+def instructions():
+
+def endStuff():
