3475f2b2a1c98b1dbcde04f503b866bc6ee238e6
oettam21 <matthew.querdasi@bush.edu>
2016-10-10 12:26:38 -0700

    Guess My Rule Game Matt and Colin

--- GuessMyRuleGame.py
+++ GuessMyRuleGame.py
@@ -1,12 +1,27 @@
 import random
 
+aValue = random.randint(1,5)
+bValue = random.randint(1,5)
 def play():
-    aValue = random.randint(1,5)
-    bValue = random.randint(1,5)
+    userValue = raw_input('Please input a sample value for the mystery rule ')
+    x = int(userValue)
+    ruleOutput = (aValue * int(userValue)) + bValue
+    print 'm*' + str(userValue) + ' + ' + 'b' + ' = ' + str(ruleOutput)
 
+play()
 
+"""
 def instructions():
+    print 'Hello!'
+    print 'Welcome to Guess my Rule Game (by Matt and Colin)'
+
+def endGame():
 
 
 def main():
+    instructions()
+    play()
+    endGame()
 
+main()
+"""
