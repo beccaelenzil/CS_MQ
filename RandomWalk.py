ca33fdb321c1647fbc5f2909cabc3f492c182183
oettam21 <matthew.querdasi@bush.edu>
2016-10-19 20:09:04 -0700

    RandomWalk Changes

--- RandomWalk.py
+++ RandomWalk.py
@@ -14,4 +14,27 @@ def rwpos(start, nsteps):
         currentPosition = currentPosition + rs()
         print 'Current position is ' + str(currentPosition)
     return currentPosition
-print rwpos(20, 7)
+#print rwpos(20, 7)
+
+
+def rwsteps(start, low, hi):
+    currentPosition = start
+    geigerCounter = 0
+    total_spaces = (hi-low)-2
+    left_spaces = low + start
+    right_spaces = hi - start
+    print '|' + left_spaces*' ' + 'X' + right_spaces*' ' + '|'+str(currentPosition)
+    while currentPosition in range(low+1, hi):
+        currentStep = rs()
+        currentPosition = currentPosition + currentStep
+        geigerCounter = geigerCounter + 1
+        if currentStep == 1:
+            right_spaces = right_spaces - 1
+            left_spaces = left_spaces + 1
+        else:
+            right_spaces = right_spaces + 1
+            left_spaces = left_spaces - 1
+        print '|' + left_spaces*' ' + 'X' + right_spaces*' ' + '|'+str(currentPosition)
+    print 'That took ' + str(geigerCounter) + ' steps'
+    return currentPosition
+print rwsteps(5,0,10)
