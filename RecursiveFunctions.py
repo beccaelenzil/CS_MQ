54644689a3b4bacbea8e6569d5499f6582f2347b
Becca Elenzil <beccaelenzil@gmail.com>
2016-10-26 11:41:02 -0700

    recursion fdbk

--- RecursiveFunctions.py
+++ RecursiveFunctions.py
@@ -3,18 +3,18 @@
 def fibIter(n):
     fibSeq = [0,1]
     if n == 1:
-        print '0'
+        #print '0'
         return 0
     elif n == 2:
-        print '0'
-        print '1'
+        #print '0'
+        #print '1'
         return 1
     else:
-        print '0'
-        print '1'
+        #print '0'
+        #print '1'
         for i in range (2,n):
             fibSeq.append(fibSeq[i-1] + fibSeq[i-2])
-            print fibSeq[-1]
+            #print fibSeq[-1]
         return fibSeq[-1]
 #fibIter(8)
 
