d8dadbe53e09b8dee50d1be3fb4dbd172a6379cb
oettam21 <matthew.querdasi@bush.edu>
2016-12-12 14:35:43 -0800

    Classwork for chapter 5

--- ScoringTheGame/chapter5tutorial.py
+++ ScoringTheGame/chapter5tutorial.py
@@ -1,21 +1,22 @@
-#LEFT OFF AT CHAPTER 5.6
+#LEFT OFF AT 5.13
 
 import pygame
+import math
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 pygame.init()
 
-BLACK = ( 0, 0, 0)
-WHITE = ( 255, 255, 255)
-GREEN = ( 0, 255, 0)
-RED = ( 255, 0, 0)
-BLUE = ( 0, 0, 255)
-WHITE = ( 0xFF, 0xFF, 0xFF)
-
-PI = 3.141592653
-
+# Set the width and height of the screen [width, height]
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 
+pygame.display.set_caption("My Game")
 
 # Loop until the user clicks the close button.
 done = False
@@ -26,17 +27,41 @@ clock = pygame.time.Clock()
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
 
     # --- Game logic should go here
 
-    # --- Drawing code should go here
+    # --- Screen-clearing code goes here
 
-    # First, clear the screen to white. Don't put other drawing commands
+    # Here, we clear the screen to white. Don't put other drawing commands
     # above this, or they will be erased with this command.
-    screen.fill(GREEN)
+
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(WHITE)
+
+    # --- Drawing code should go here
+    """
+    pygame.draw.circle(screen, RED, [100, 100], 30)
+    pygame.draw.circle(screen, BLUE, [200, 100], 30)
+    pygame.draw.line(screen, GREEN, [100, 100], [100, 250], 5)
+    pygame.draw.lines(screen, GREEN, False, [[100, 100], [150, 200], [200, 100], [200, 250]], 5)
+
+    for y_offset in range(0, 100, 7):
+        pygame.draw.line(screen,RED,[0,10+y_offset],[100,110+y_offset],5)
+    """
+    for i in range(200):
+        radians_x = i / 20
+        radians_y = i / 6
+        x = int(75 * math.sin(radians_x)) + 200
+        y = int(75 * math.cos(radians_y)) + 200
+        pygame.draw.line(screen, BLACK, [x,y], [x+5,y], 5)
+
+    for x_offset in range(30, 300, 30):
+        pygame.draw.line(screen,BLACK,[x_offset,100],[x_offset-10,90],2)
+        pygame.draw.line(screen,BLACK,[x_offset,90],[x_offset-10,100],2)
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
@@ -44,3 +69,5 @@ while not done:
     # --- Limit to 60 frames per second
     clock.tick(60)
 
+# Close the window and quit.
+pygame.quit()
