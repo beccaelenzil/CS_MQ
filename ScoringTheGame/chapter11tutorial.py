2f64c073c95b4595254d7bf58d9147661ad74440
oettam21 <matthew.querdasi@bush.edu>
2016-12-13 09:48:39 -0800

    Classwork on chapter11 tutorial

--- ScoringTheGame/chapter11tutorial.py
+++ ScoringTheGame/chapter11tutorial.py
@@ -1,3 +1,54 @@
 
 import pygame
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+PI = 3.141592653
+
+pygame.init()
+
+size = (1000, 650)
+screen = pygame.display.set_mode(size)
+
+background_duck = pygame.image.load("Daffy_Duck.png").convert()
+
+player_image = pygame.image.load("Donald-duck.png").convert()
+
+player_image.set_colorkey(RED)
+
+done = False
+
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done = True # Flag that we are done so we exit this loop
+
+    # --- Game logic should go here
+
+    # --- Drawing code should go here
+    screen.blit(background_duck, [-150, -150])
+
+    player_position = pygame.mouse.get_pos()
+    x = player_position[0]
+    y = player_position[1]
+
+    screen.blit(player_image, [x-120, y-150])
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+
