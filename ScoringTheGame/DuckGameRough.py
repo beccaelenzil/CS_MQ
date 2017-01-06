333149430ddfeb4275f9766b0940fb3aaa1b8807
oettam21 <matthew.querdasi@bush.edu>
2017-01-06 11:48:38 -0800

    Shooting duck game changes

--- ScoringTheGame/DuckGameRough.py
+++ ScoringTheGame/DuckGameRough.py
@@ -6,6 +6,7 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+ORANGE = (255, 255, 0)
 
 PI = 3.141592653
 
@@ -36,6 +37,8 @@ y_speed = 0
 shot_y_speed = 0
 shot_y1_speed = 0
 shot_y2_speed = 0
+shot_y3_speed = 0
+shot_y4_speed = 0
 
 # Current position
 x_coord = 460
@@ -50,6 +53,12 @@ shot_y1_coord = 560
 
 shot_x2_coord = 460
 shot_y2_coord = 560
+
+shot_x3_coord = 460
+shot_y3_coord = 560
+
+shot_x4_coord = 460
+shot_y4_coord = 560
 
 LEFT = True
 RIGHT = True
@@ -88,10 +97,6 @@ while not done:
                 x_speed += -10
             if event.key == pygame.K_RIGHT and x_coord <= 990:
                 x_speed += 10
-            if event.key == pygame.K_UP and y_coord >= 0:
-                y_speed += -15
-            if event.key == pygame.K_DOWN and y_coord <= 640:
-                y_speed += 5
 
             #Shot code for shots
             if event.key == pygame.K_SPACE and SHOT == False:
@@ -100,12 +105,30 @@ while not done:
                     shot_y_speed += -10
                 else:
                     shot_y_speed = -10
-            elif event.key == pygame.K_SPACE and SHOT == True:
+            elif event.key == pygame.K_SPACE and SHOT1 == False:
                 SHOT1 = True
                 if shot_y1_speed > -10:
                     shot_y1_speed += -10
                 else:
                     shot_y1_speed = -10
+            elif event.key == pygame.K_SPACE and SHOT2 == False:
+                SHOT2 = True
+                if shot_y2_speed > -10:
+                    shot_y2_speed += -10
+                else:
+                    shot_y2_speed = -10
+            elif event.key == pygame.K_SPACE and SHOT3 == False:
+                SHOT3 = True
+                if shot_y3_speed > -10:
+                    shot_y3_speed += -10
+                else:
+                    shot_y3_speed = -10
+            elif event.key == pygame.K_SPACE and SHOT4 == False:
+                SHOT4 = True
+                if shot_y4_speed > -10:
+                    shot_y4_speed += -10
+                else:
+                    shot_y4_speed = -10
 
 
 
@@ -133,7 +156,7 @@ while not done:
     x_coord += x_speed
     y_coord += y_speed
 
-    #shot1
+    #shot
     if SHOT == False:
         shot_y_coord += y_speed
         shot_x_coord += x_speed
@@ -143,7 +166,7 @@ while not done:
         SHOT = False
         shot_y_coord = y_coord
         shot_x_coord = x_coord
-    #shot2
+    #shot1
     if SHOT1 == False:
         shot_y1_coord += y_speed
         shot_x1_coord += x_speed
@@ -153,6 +176,36 @@ while not done:
         SHOT1 = False
         shot_y1_coord = y_coord
         shot_x1_coord = x_coord
+    #shot2
+    if SHOT2 == False:
+        shot_y2_coord += y_speed
+        shot_x2_coord += x_speed
+    elif SHOT2 == True:
+        shot_y2_coord += shot_y2_speed
+    if shot_y2_coord <= 0:
+        SHOT2 = False
+        shot_y2_coord = y_coord
+        shot_x2_coord = x_coord
+    #shot3
+    if SHOT3 == False:
+        shot_y3_coord += y_speed
+        shot_x3_coord += x_speed
+    elif SHOT3 == True:
+        shot_y3_coord += shot_y3_speed
+    if shot_y3_coord <= 0:
+        SHOT3 = False
+        shot_y3_coord = y_coord
+        shot_x3_coord = x_coord
+    #shot4
+    if SHOT4 == False:
+        shot_y4_coord += y_speed
+        shot_x4_coord += x_speed
+    elif SHOT4 == True:
+        shot_y4_coord += shot_y4_speed
+    if shot_y4_coord <= 0:
+        SHOT4 = False
+        shot_y4_coord = y_coord
+        shot_x4_coord = x_coord
 
     if x_coord < 0:
         x_coord = 0
@@ -168,6 +221,18 @@ while not done:
         shot_x1_coord = 0
     if shot_x1_coord > 940:
         shot_x1_coord = 940
+    if shot_x2_coord < 0:
+        shot_x2_coord = 0
+    if shot_x2_coord > 940:
+        shot_x2_coord = 940
+    if shot_x3_coord < 0:
+        shot_x3_coord = 0
+    if shot_x3_coord > 940:
+        shot_x3_coord = 940
+    if shot_x4_coord < 0:
+        shot_x4_coord = 0
+    if shot_x4_coord > 940:
+        shot_x4_coord = 940
 
     print x_coord,y_coord
     print shot_y_coord
@@ -175,16 +240,28 @@ while not done:
         print "SHOT"
     if SHOT1 == True:
         print "SHOT 111"
+    if SHOT2 == True:
+        print "SHOT 222"
+    if SHOT3 == True:
+        print "SHOT 333"
+    if SHOT4 == True:
+        print "SHOT 444"
     #defining the ball that shoots
     def shooting_motion(screen, x, y):
         #Shot
         pygame.draw.ellipse(screen, BLUE, [1+x,y,20,20], 0)
     def shooting_motion1(screen, x, y):
         #Shot
-        pygame.draw.ellipse(screen, BLUE, [1+x,y,20,20], 0)
+        pygame.draw.ellipse(screen, RED, [1+x,y,20,20], 0)
     def shooting_motion2(screen, x, y):
         #Shot
-        pygame.draw.ellipse(screen, BLUE, [1+x,y,20,20], 0)
+        pygame.draw.ellipse(screen, GREEN, [1+x,y,20,20], 0)
+    def shooting_motion3(screen, x, y):
+        #Shot
+        pygame.draw.ellipse(screen, BLACK, [1+x,y,20,20], 0)
+    def shooting_motion4(screen, x, y):
+        #Shot
+        pygame.draw.ellipse(screen, ORANGE, [1+x,y,20,20], 0)
 
     # --- Game logic should go here
     """
@@ -196,9 +273,12 @@ while not done:
     # --- Drawing code should go here
     screen.fill(WHITE)
 
-    shooting_motion(screen, shot_x_coord+23, shot_y_coord-15)
-    shooting_motion1(screen, shot_x1_coord+23, shot_y1_coord-15)
-    shooting_motion2(screen, shot_x_coord+23, shot_y_coord-15)
+    shooting_motion(screen, shot_x_coord+23, shot_y_coord-5)
+    shooting_motion1(screen, shot_x1_coord+23, shot_y1_coord-5)
+    shooting_motion2(screen, shot_x2_coord+23, shot_y2_coord-5)
+    shooting_motion3(screen, shot_x3_coord+23, shot_y3_coord-5)
+    shooting_motion4(screen, shot_x4_coord+23, shot_y4_coord-5)
+
 
     screen.blit(duck_shooter, [x_coord, y_coord])
 
