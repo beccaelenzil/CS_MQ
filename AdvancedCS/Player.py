6513269426ace8754805cc54d7f15e471af00ae8
oettam21 <matthew.querdasi@bush.edu>
2017-04-12 10:03:27 -0700

    changes

--- AdvancedCS/Player.py
+++ AdvancedCS/Player.py
@@ -1,3 +1,4 @@
+import random
 from Board import Board
 
 class Player():
@@ -33,9 +34,49 @@ class Player():
             score = 50.0
         return score
 
+    #takes in scores list and returns column number of highest scores resulting from possible moves
     def tiebreakMove(self, scores):
+        maxIndices = []  #list with scores
 
-        max(scores)
+        for i in range(len(scores)):  #goes through list for how many scores there are
+            if scores[i] == max(scores):  #finds highest scores and appends to maxIndices
+                maxIndices.append(i)
+
+        #taking tie break type and implementing left or right move, if only there is more than one high score
+        if len(maxIndices) > 1:
+            if self.tbt == 'RIGHT':
+                return maxIndices[len(maxIndices)-1]
+            elif self.tbt == 'LEFT':
+                return maxIndices[0]
+            else:
+                return maxIndices[random.randrange(len(maxIndices))]
+        else:
+            return maxIndices[0]
+
+
+    def scoresFor(self, b):
+        scores = []
+
+        for col in range(b.width):
+            b.addMove(col, self.ox)
+            if b.allowsMove(col):
+                if self.scoreBoard(b) == 100.0:
+                    scores.append(self.scoreBoard(b))
+                else:
+                    b.addMove(col, self.oppCh())
+                    scores.append(self.scoreBoard(b))
+                    b.delMove(col)
+            else:
+                scores.append(-1)
+            b.delMove(col)
+
+        for col in range(b.width):
+            b.addMove(col, self.oppCh())
+            if scores[col] == 50.0:
+                scores[col] = self.scoreBoard(b)
+            b.delMove(col)
+
+        return scores
 
 
 b = Board(7,6)
@@ -50,3 +91,10 @@ print Player('O', 'LEFT', 0).scoreBoard(b)
 
 print Player('O', 'LEFT', 0).scoreBoard(Board(7,6))
 
+
+scores = [0, 0, 50, 0, 50, 50, 0]
+p = Player('X', 'LEFT', 1)
+p2 = Player('X', 'RIGHT', 1)
+print p.tiebreakMove(scores)
+
+print p2.tiebreakMove(scores)
