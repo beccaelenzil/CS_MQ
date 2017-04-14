006e6b9375a9e290d89b88cbb5c1e747e2101844
oettam21 <matthew.querdasi@bush.edu>
2017-04-14 11:45:43 -0700

    quick sort

--- AdvancedCS/Sort.py
+++ AdvancedCS/Sort.py
@@ -1,3 +1,13 @@
+#SORT
+# File: Sort.py
+# Verions: Python 2.7.13
+# Name: Matt Querdasi
+# Date: 4/13/17
+# Desc: PROG DESC
+# Usage: Sort class
+
+import random
+
 def mergeSort(alist):
     print "Splitting ",alist
     if len(alist)>1:
@@ -31,19 +41,34 @@ def mergeSort(alist):
             k=k+1
     print "Merging ",alist
 
+"""
 alist = [54,26,93,17,77,31,44,55,20]
 mergeSort(alist)
 print(alist)
+"""
 
-
-def my_quicksort(some_list, start, stop):
+def quickSort(some_list, start, stop):
     if stop - start < 1:
         return some_list
+
     else:
         pivot = some_list[start]
-        left = start
-        right = stop
-        while left <= right:
-            while some_list[left] < pivot:
-                left += 1
 
+        while start <= stop:
+            while some_list[start] < pivot:
+                start += 1
+            while some_list[stop] > pivot:
+                stop -= 1
+
+            if start <= stop:
+                some_list[start], some_list[stop] = some_list[stop], some_list[start]
+                start += 1
+                stop -= 1
+
+        print some_list
+
+        quickSort(some_list, start, stop)
+        quickSort(some_list, start, stop)
+
+my_list = [39, 30, 45, 33, 20, 61, 36, 5, 31, 64]
+quickSort(my_list, 0, len(my_list) - 1)
